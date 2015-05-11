0decf26818f4b304077e5f4afce3a85c59959c49
Marek Goldmann <marek.goldmann@gmail.com>
2015-05-11 09:23:18 -0400

    Add more info to setup.py before release

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ setup(
     name = "docker-scripts",
     version = "0.3.0",
     packages = find_packages(),
+    url='https://github.com/goldmann/docker-scripts',
+    author='Marek Goldmann',
+    author_email='marek.goldmann@gmail.com',
+    license='MIT',
     entry_points={
         'console_scripts': ['docker-scripts=docker_scripts.cli.main:run'],
     },

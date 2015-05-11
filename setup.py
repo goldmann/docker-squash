5c929cd84074d9fe17548afec4e6e81ad61416ae
Marek Goldmann <marek.goldmann@gmail.com>
2015-05-11 08:46:36 -0400

    Use requirements.txt file for... requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name = "docker-scripts",
-    version = "0.2.2",
+    version = "0.3.0",
     packages = find_packages(),
     entry_points={
         'console_scripts': ['docker-scripts=docker_scripts.cli.main:run'],
     },
+    install_requires=requirements
 )

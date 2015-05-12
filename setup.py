023c0f17652037c80c9513a8ab522373d7ccae9d
Marek Goldmann <marek.goldmann@gmail.com>
2015-05-12 05:19:24 -0400

    Add download_url to setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@
 
 from setuptools import setup, find_packages
 
+version = "0.3.1"
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name = "docker-scripts",
-    version = "0.3.1",
+    version = version,
     packages = find_packages(),
-    url='https://github.com/goldmann/docker-scripts',
-    author='Marek Goldmann',
-    author_email='marek.goldmann@gmail.com',
+    url = 'https://github.com/goldmann/docker-scripts',
+    download_url = "https://github.com/goldmann/docker-scripts/archive/%s.tar.gz" % version,
+    author = 'Marek Goldmann',
+    author_email = 'marek.goldmann@gmail.com',
     description = 'A swiss-knife tool that could be useful for people working with Docker',
     license='MIT',
     keywords = 'docker',
-    long_description=open('README.rst').read(),
-    entry_points={
+    long_description = open('README.rst').read(),
+    entry_points = {
         'console_scripts': ['docker-scripts=docker_scripts.cli:run'],
     },
     install_requires=requirements
